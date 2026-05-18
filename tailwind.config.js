ccba9c4e3cce14cceee7cc5e88a4361e31bab467
michaelbiz1618-pixel <michael.biz1618@gmail.com>
2026-05-18 17:38:05 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "Arial", "sans-serif"]
+      },
+      colors: {
+        rational: "#0A3A5A",
+        level1: "#1F5C85",
+        level2: "#4F7FA3",
+        surface: "#E6EEF4",
+        neutralText: "#2E2E2E"
+      }
+    }
+  },
+  plugins: []
+};
